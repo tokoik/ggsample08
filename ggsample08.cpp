dbc8b3b27ac0a2e6fd08d8f25b8d26f37cf7e876
Kohe Tokoi <tokoik@gmail.com>
2021-07-16 12:03:15 +0900

    Separate window-related processing into Window.h

--- ggsample08.cpp
+++ ggsample08.cpp
@@ -3,11 +3,13 @@
 //
 #include "GgApp.h"
 
+// プロジェクト名
+#ifndef PROJECT_NAME
+#  define PROJECT_NAME "ggsample08"
+#endif
+
 // シェーダー関連の処理
 #include "shader.h"
-
-// 標準ライブラリ
-#include <cmath>
 
 // アニメーションの周期（秒）
 constexpr auto cycle{ 5.0 };
@@ -86,8 +88,8 @@ static void makeSphere(float radius, int slices, int stacks,
 //
 int GgApp::main(int argc, const char* const* argv)
 {
-  // ウィンドウを作成する
-  Window window{ "ggsample08" };
+  // ウィンドウを作成する (この行は変更しないでください)
+  Window window{ argc > 1 ? argv[1] : PROJECT_NAME };
 
   // 背景色を指定する
   glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
@@ -97,7 +99,7 @@ int GgApp::main(int argc, const char* const* argv)
   glEnable(GL_CULL_FACE);
 
   // プログラムオブジェクトの作成
-  const auto program{ loadProgram("ggsample08.vert", "pv", "ggsample08.frag", "fc") };
+  const auto program{ loadProgram(PROJECT_NAME ".vert", "pv", PROJECT_NAME ".frag", "fc") };
 
   // in (attribute) 変数のインデックスの検索（見つからなければ -1）
   const auto nvLoc{ glGetAttribLocation(program, "nv") };
